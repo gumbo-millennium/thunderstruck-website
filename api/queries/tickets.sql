421d7c8ad5b7a45d2355ae9ce314f9227dccdca4
bjvanbemmel <beauvanbemmel@live.nl>
2025-11-16 17:11:15 +0100

    add state and value to sqlc queries

--- api/queries/tickets.sql
+++ api/queries/tickets.sql
@@ -1,27 +1,29 @@
 -- name: GetOne :one
-SELECT id, type, email, created_at, updated_at, deleted_at
+SELECT id, type, state, email, value, created_at, updated_at, deleted_at
 FROM tickets
 WHERE id = $1 AND deleted_at IS NULL
 LIMIT 1;
 
 -- name: GetAll :many
-SELECT id, type, email, created_at, updated_at, deleted_at
+SELECT id, type, state, email, value, created_at, updated_at, deleted_at
 FROM tickets
 WHERE deleted_at IS NULL;
 
 -- name: Create :one
 INSERT INTO tickets (
-  id, type, email, updated_at, deleted_at
+  type, state, email, value, updated_at, deleted_at
 )
 VALUES (
-  $1, $2, $3, $4, $5
+  $1, $2, $3, $4, $5, $6
 )
 RETURNING *;
 
 -- name: Update :one
 UPDATE tickets SET
   type = $2,
-  email = $3,
+  state = $3,
+  email = $4,
+  value = $5,
   updated_at = NOW()
 WHERE id = $1
 RETURNING *;
